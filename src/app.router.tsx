6473d65bdfdded36458e8c885ec01a2c1cfb5ebb
cdeorta <cdeorta.dev@gmail.com>
2026-01-19 14:50:56 -0300

    hash router

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import {createBrowserRouter, Navigate} from 'react-router'
+import {createBrowserRouter, createHashRouter, Navigate} from 'react-router'
 import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
 import { ProductPage } from './shop/pages/product/ProductPage';
@@ -18,8 +18,9 @@ import { AdminRoute, NotAuthenticatedRoute } from './components/routes/Protected
 const AuthLayout = lazy(()=>import('./auth/layouts/AuthLayout'));
 const AdminLayout = lazy(()=>import('./admin/layouts/AdminLayout'));
 
-export const appRouter = createBrowserRouter([
+// export const appRouter = createBrowserRouter([
     
+    export const appRouter = createHashRouter([
     
     {
         path:'/',
